b597dc1e9ec52c3650bebbd55d4ad2798e137676
Carlos Pereira Atencio <carlos@microbit.org>
2024-11-29 17:34:15 +0000

    Change dB lower value linear decay to non-linear.

--- soundleveldb.cpp
+++ soundleveldb.cpp
@@ -22,37 +22,37 @@ int soundLevelDbOriginal() {
 }
 
 
-int soundLevelDbF(int value) {
+int scaleLowerDbValues(int value) {
     // From values about 70 db y is accurate, for values below we need to weight it
     if (value > 65) {
         return value;
     }
 
-    const int MIN_DB = 36;
-    const int MIN_DB_Y = 52;
-    const int MAX_DB = 66;
+    const int MAX_DB = 65;
+    const int MIN_DB = 30;
+    const int MIN_VALUE = 51;
+    const int RANGE_VALUE = MAX_DB - MIN_VALUE;
     const int RANGE_DB = MAX_DB - MIN_DB;
-    const int RANGE_DB_Y = MAX_DB - MIN_DB_Y;
-    const float EXTRA_DECAY = (float)RANGE_DB / (float)RANGE_DB_Y;
 
-    // Between Y being 64 and 52 we decay three times faster towards 28
-    return (int)MAX_DB - ((MAX_DB - value) * EXTRA_DECAY);
+    // Original linear decay
+    // return (int)MAX_DB - ((MAX_DB - value) * EXTRA_DECAY);
+
+    float fraction = (float)(value - MIN_VALUE) / (float)RANGE_VALUE;
+    float nonLinearFraction = sqrtf(fraction);
+    return MIN_DB + (int)(RANGE_DB * nonLinearFraction);
 }
-
-static LevelDetectorSPL *localLevelSPL = NULL;
-
-#define DEVICE_ID_UNUSED 50
-
 
 //% blockId=device_get_sound_level_db
 int soundLevelDbC() {
 #if MICROBIT_CODAL
+    static LevelDetectorSPL *localLevelSPL = NULL;
+    const int DEVICE_ID_UNUSED = 50;
     if (localLevelSPL == NULL) {
+        // Lower gain and lower min value than default uBit.audio.levelSPL
         localLevelSPL = new LevelDetectorSPL(*(uBit.audio.rawSplitter->createChannel()), 200.0, 1.0, 12.0, 50.0, DEVICE_ID_UNUSED, true);
-        //localLevelSPL = new LevelDetectorSPL(uBit.audio.processor->output, 200.0, 1.0, 1.0, 30.0, DEVICE_ID_UNUSED, true);
     }
-    int detector_db = (int)localLevelSPL->getValue(LEVEL_DETECTOR_SPL_DB);
-    return soundLevelDbF(detector_db);
+    int localLevelValue = localLevelSPL->getValue(LEVEL_DETECTOR_SPL_DB);
+    return scaleLowerDbValues(localLevelValue);
 #else
     target_panic(PANIC_VARIANT_NOT_SUPPORTED);
     return 0;
