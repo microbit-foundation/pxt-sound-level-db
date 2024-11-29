9f27aaa881bd419c8a072e72136b2efbea7ca2cb
Carlos Pereira Atencio <carlos@microbit.org>
2024-11-29 19:22:30 +0000

    Set flag to ignore V1 compilation, its cloud compiler fails due to parenthesis in filenames.

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
+let maxSound = 0
+let soundLevel = 0
 basic.forever(function () {
-    let dbOriginal = input.soundLevelDbOriginal()
-    let dbFinal = input.soundLevelDb()
-    serial.writeLine("" + dbOriginal + ", " + dbFinal)
-    basic.pause(100)
+    soundLevel = input.soundLevelDb()
+    led.plotBarGraph(
+    Math.map(soundLevel, 30, 120, 0, 25),
+    25
+    )
+    if (soundLevel > maxSound) {
+        maxSound = soundLevel
+    }
 })
+
 input.onButtonPressed(Button.A, function () {
+    basic.showNumber(maxSound)
+})
+input.onButtonPressed(Button.B, function () {
     basic.showNumber(input.soundLevelDb())
 })
