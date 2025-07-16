9eb6ecba9fd995542d818b182295f3b2b588ad39
Carlos Pereira Atencio <carlos@microbit.org>
2025-07-16 18:00:03 +0100

    Add workaround for CODAL v0.3.0.

--- soundleveldb.cpp
+++ soundleveldb.cpp
@@ -10,13 +10,20 @@ namespace input {
 // https://github.com/lancaster-university/codal-microbit-v2/issues/489
 // It has been deployed to MakeCode live on the 16th of July 2025.
 // The fix should be released with CODAL v0.3.2.
-// In the meantime, the extension will continue to use the workaround.
+// Until that is released, this extension will continue to use the workaround.
 #if (!defined(CODAL_VERSION_MAJOR) || !defined(CODAL_VERSION_MINOR)) || (CODAL_VERSION_MAJOR < 1 && CODAL_VERSION_MINOR < 3)
     // #warning "COMPILING PRE_REFACTOR"
     #define CODAL_PRE_SOUND_REFACTOR 1
 #else
     // #warning "COMPILING REFACTORED"
     #define CODAL_PRE_SOUND_REFACTOR 0
+
+    // There is a specific workaround for v0.3.0, deployed to MakeCode live.
+    #if CODAL_VERSION_MAJOR == 0 && CODAL_VERSION_MINOR == 3 && CODAL_VERSION_PATCH == 0
+        #define CODAL_0_3_0 1
+    #else
+        #define CODAL_0_3_0 0
+    #endif
 #endif
 
 int preRefactorScaleLowerDbValues(int value) {
@@ -56,10 +63,37 @@ int soundLevelDbC() {
                 localLevelSPL = new LevelDetectorSPL(*(uBit.audio.rawSplitter->createChannel()), 200.0, 1.0, 12.0, 30.0, DEVICE_ID_UNUSED);
             #endif
         }
+
+        #if CODAL_0_3_0 == 1
+            // UGLY WORKAROUND FOR CODAL v0.3.0 deployed to MakeCode live.
+            // A LevelDetectorSPL can lock up waiting for data, but calling it multiple
+            // times in a row will eventually return values.
+            // So, launch a few fibers, they all yield to each other until they all finish.
+            static bool workaroundInitialised = false;
+            if (!workaroundInitialised) {
+                static volatile int fibersDone = 0;
+                const int totalFibers = 10;
+                for (int i = 0; i < totalFibers; i++) {
+                    create_fiber([](void *param) {
+                        uBit.sleep(1);
+                        uBit.audio.levelSPL->getValue(LEVEL_DETECTOR_SPL_8BIT);
+                        fibersDone++;
+                        uBit.sleep(1);
+                        //uBit.serial.printf("Fiber done (%d).\n", fibersDone);
+                    }, (void *)fibersDone, release_fiber);
+                    uBit.sleep(1);
+                }
+                while (fibersDone < totalFibers) {
+                    uBit.sleep(10);
+                }
+                workaroundInitialised = true;
+            }
+        #endif
+
         int localLevelValue = localLevelSPL->getValue(LEVEL_DETECTOR_SPL_DB);
         return preRefactorScaleLowerDbValues(localLevelValue);
 
-        // In the future, CODAL refactor from v0.3.0 can just call this instead
+        // In the future, CODAL >= v0.3.1 can just call this instead
         // return uBit.audio.levelSPL->getValue(LEVEL_DETECTOR_SPL_DB);
 #else
     target_panic(PANIC_VARIANT_NOT_SUPPORTED);
@@ -67,4 +101,4 @@ int soundLevelDbC() {
 #endif
 }
 
-}
+} // namespace input
